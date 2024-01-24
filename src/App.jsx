817ea37850e5fddeeb95f2d6c62fd6f9ca1daf82
Hailekeh22 <hailekjt@gmail.com>
2024-01-24 21:04:43 +0300

    Initial App

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Hero from "./Components/Hero/Hero";
+import Body from "./Components/Body/Body";
+import Footer from "./Components/Footer/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Body />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Body/Body.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Body.css";
+
+function Body() {
+  return <div>Body</div>;
+}
+
+export default Body;
