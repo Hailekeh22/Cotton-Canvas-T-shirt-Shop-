579a8f3870e2c663b76ee60d2153c8531cbe94b7
Hailekeh22 <hailekjt@gmail.com>
2024-01-25 11:30:04 +0300

    Nav-bar finished

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
+import Logo from "../assets/Logotransparent.png";
+import { FaCartShopping } from "react-icons/fa6";
 
 function Header() {
-  return <div>Header</div>;
+  const [count, setcount] = useState(0);
+
+  return (
+    <div className="Header">
+      <div className="logo-container">
+        <img className="logo" src={Logo} />
+      </div>
+      <div className="cart">
+        <button className="cart-container">
+          <FaCartShopping size={"1rem"} /> <h4 className="cart-txt">Cart</h4>{" "}
+          <div className="cart-count">{count}</div>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
