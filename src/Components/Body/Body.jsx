04d30e69fd2f4ab09b34659853809611c7c645f5
Hailekeh22 <hailekjt@gmail.com>
2024-01-27 19:37:43 +0300

    Added Body section

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import "./Body.css";
 
 function Body() {
-  return <div>Body</div>;
+  return (
+    <div>
+      <div className="Tittle-section">
+        <h3>Our Products </h3>
+      </div>
+      <div className="Products-list">
+        <div className="Item-toogle">
+          <button>Men</button>
+          <button className="women-item">Women</button>
+        </div>
+        <div className="Item-view">hello</div>
+      </div>
+    </div>
+  );
 }
 
 export default Body;
