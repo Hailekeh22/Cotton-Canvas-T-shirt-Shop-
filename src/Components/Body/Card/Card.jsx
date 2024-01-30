473df3f2d2982f3ebf2c439ed0b2db1ae9a0da20
Hailekeh22 <hailekjt@gmail.com>
2024-01-30 21:19:04 +0300

    item value modefied

--- src/Components/Body/Card/Card.jsx
+++ src/Components/Body/Card/Card.jsx
@@ -1,47 +1,16 @@
 import React from "react";
 import "./Card.css";
-import Img1 from "../Card/M1.jpg";
 
-function Card() {
+function Card(props) {
   return (
     <div className="card-container">
       <div className="Card-item">
         <div className="Card-image">
-          <img className="img-item" src={Img1}></img>
+          <img className="img-item" src={props.image}></img>
         </div>
         <div className="Card-Content">
-          <h3>White Tshirt</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-          <button className="addtocart-button">Add to Cart</button>
-        </div>
-      </div>
-      <div className="Card-item">
-        <div className="Card-image">
-          <img className="img-item" src={Img1}></img>
-        </div>
-        <div className="Card-Content">
-          <h3>White Tshirt</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-          <button className="addtocart-button">Add to Cart</button>
-        </div>
-      </div>
-      <div className="Card-item">
-        <div className="Card-image">
-          <img className="img-item" src={Img1}></img>
-        </div>
-        <div className="Card-Content">
-          <h3>White Tshirt</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-          <button className="addtocart-button">Add to Cart</button>
-        </div>
-      </div>
-      <div className="Card-item">
-        <div className="Card-image">
-          <img className="img-item" src={Img1}></img>
-        </div>
-        <div className="Card-Content">
-          <h3>White Tshirt</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+          <h3>{props.name}</h3>
+          <p>{props.info}</p>
           <button className="addtocart-button">Add to Cart</button>
         </div>
       </div>
